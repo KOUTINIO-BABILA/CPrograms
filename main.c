860c14ee832210cb2ad24045921c51fa4f4327cb
MOHAMADOU LAMO BABILA <babilakouti@gmail.com>
2025-12-19 02:47:10 +0100

    FirstChanges

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1, n2, sum;
+
+    printf("Enter the first num");
+    scanf("%d", &n1);
+
+    printf("Enter the second num");
+    scanf("%d", &n2);
+    
+    sum = n1+n2;
+
+    printf("The sum of this 02 number is : %d", sum);
+    return 0;
+}
